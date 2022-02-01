cc660151dcec7867f8d5c543c36f111520316c72
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-02 02:20:56 +0300

    reference typef variables

--- 02/02 Variables Types/referenceTypesVariables.js
+++ 02/02 Variables Types/referenceTypesVariables.js
@@ -1,1 +1,22 @@
 /*Reference types of variables*/
+/*These types sees as Objects like array. */
+console.log("---");
+//Array
+let names = ["Recep", "Mehmet", "Şeyda"];
+console.log(typeof names);
+//Above code wil show the object in the console.
+
+//Object
+let address = {
+  city: "Elbistan",
+  country: "Turkey",
+};
+console.log(typeof address);
+//Above code wil show the object in the console.
+
+//Function
+let returnValue = function () {
+  return 0;
+};
+console.log(typeof returnValue);
+//Above code wil show the function in the console.
