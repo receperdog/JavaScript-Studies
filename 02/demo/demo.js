0108e41c4addb1753b3e98bf64eda04d74056ff1
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-02 03:04:11 +0300

    basic demo

---
+++ 02/demo/demo.js
@@ -0,0 +1,17 @@
+let customer = {
+  name: "Recep",
+  surName: "Erdogan",
+  idNumber: 1234,
+  gender: "male",
+  address: "Antalya",
+  orderAmount: 500,
+};
+console.log(
+  "Hello Mr." +
+    " " +
+    customer.name +
+    " " +
+    "you have to pay " +
+    customer.orderAmount +
+    " tl"
+);
