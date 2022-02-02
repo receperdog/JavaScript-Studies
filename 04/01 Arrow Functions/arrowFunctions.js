c93edbdb677236820d6db8b5146276ca46f04435
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-02 23:53:00 +0300

    basics of arrow functions

---
+++ 04/01 Arrow Functions/arrowFunctions.js
@@ -0,0 +1,11 @@
+//Arrow functions properties comes with ES6. Let's dive in.
+let multiply = (numberOne, numberTwo) => numberOne * numberTwo;
+console.log(multiply(2, 2));
+
+let concatenationOfTwoStrings = (stringOne, stringTwo) =>
+  stringOne + " " + stringTwo;
+console.log(concatenationOfTwoStrings("Recep", "Erdogan"));
+
+let addNumbers = (number, numberTwo, numberThree) =>
+  number + numberTwo + numberThree;
+console.log(addNumbers(1, 2, 3));
