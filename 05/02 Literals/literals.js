d47ea424d4964e23dd40142cb11a38f54f9a0e56
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-04 03:22:52 +0300

    literals

---
+++ 05/02 Literals/literals.js
@@ -0,0 +1,10 @@
+/** Literals represent values in JavaScript. These are fixed values—not variables—that you literally provide in your script.
+ * source: https://developer.mozilla.org/en-US/docs/Glossary/Literal
+ */
+//Object Literals
+let car = {
+    myCar: 'Clio',
+    price: 60000,
+    color: 'Gray'
+}
+console.log(`My car model is ${car.myCar} and it's price is ${car.price}`);
